ff277ea2e1b20a8320b4c122c86750155b271789
Lívio Figueiredo <livobf@gmail.com>
2018-06-24 16:13:46 -0300

    Ainda faltando o atributo Aluno aluno.

---
+++ src/java/DAO/Contrato.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author lfigueiredo
+ */
+public class Contrato {
+    private int id;
+    private Vaga vaga;
+    private int aprovado;
+    private Coordenador coordenador;
+}
