22dbecb7301e4e4ab9845d8d385fd1729fc2875e
Shiaku <eduardosierra@id.uff.br>
2018-06-30 15:19:10 -0300

    +adding constructor and access methods.

--- src/java/DAO/Contrato.java
+++ src/java/DAO/Contrato.java
@@ -1,17 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author lfigueiredo
- */
-public class Contrato {
+public class Contrato 
+{
     private int id;
+    private Aluno aluno;
     private Vaga vaga;
-    private int aprovado;
+    private boolean aprovado;
     private Coordenador coordenador;
+    
+    public Contrato(int id, Aluno aluno, Vaga vaga)
+    {
+        this.id = id;
+        this.aluno = aluno;
+        this.vaga = vaga;
+        this.aprovado = false;
+        this.coordenador = null;
+    }
+    
+    public int GetID()
+    {
+        return id;
+    }
+    
+    public Vaga GetVaga()
+    {
+        return vaga;
+    }
+    
+    public boolean EstaAprovado()
+    {
+        return aprovado;
+    }
+    
+    public void Aprova(Coordenador coordenador)
+    {
+        aprovado = true;
+        this.coordenador = coordenador;
+    }
+    
+    public Coordenador GetCoordenador()
+    {
+        return coordenador;
+    }
 }
