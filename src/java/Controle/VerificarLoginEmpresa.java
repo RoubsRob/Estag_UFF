3426bbe266f749939f5eb1bfc16977e1cb36ba7d
Henrique <hndmedeiros@gmail.com>
2018-07-16 20:31:24 -0300

    servlet danado

--- src/java/Controle/VerificarLoginEmpresa.java
+++ src/java/Controle/VerificarLoginEmpresa.java
@@ -5,13 +5,19 @@
  */
 package Controle;
 
+import DAO.EmpresaDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -19,70 +25,51 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "VerificarLoginEmpresa", urlPatterns = {"/VerificarLoginEmpresa"})
 public class VerificarLoginEmpresa extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet VerificarLoginEmpresa</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet VerificarLoginEmpresa at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+try{
+       System.out.println("Entrou no servlet VerificarLoginEmpresa");
+        String nome_user = request.getParameter("loginEmpresa");
+        String senha_user = request.getParameter("senha");
+        
+        
+        
+        try {
+              if(EmpresaDAO.SenhaCorreta(nome_user, senha_user)){  
+                
+                  System.out.println("senha aluno correta");
+                  
+                  
+                HttpSession session = request.getSession();
+                session.setAttribute("NomeEmpresaLogado", nome_user);
+                session.setAttribute("logado", "ok");
+                
+                
+                Cookie meuCookie = new Cookie("Nome", nome_user);
+                meuCookie.setMaxAge(60*30);
+                response.addCookie(meuCookie);
+                
+                RequestDispatcher resposta = request.getRequestDispatcher("/menuLogadoEmpresa.jsp");
+                resposta.forward(request, response);
+            } else {
+                response.sendRedirect("index.jsp");
+            }
+        } catch (SQLException ex) {
+            RequestDispatcher resposta = request.getRequestDispatcher("/erro.jsp");
+            resposta.forward(request, response);
+            Logger.getLogger(VerificarLoginEmpresa.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }catch (Exception e){
+    System.out.println("ERRO NO LOGIN");
+    RequestDispatcher resposta = request.getRequestDispatcher("/erro.jsp");
+    resposta.forward(request, response);
+    }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                response.sendRedirect("index.jsp");
+    }
 
 }
