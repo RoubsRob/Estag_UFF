93b862d14933f3139d3c0f8e03c7d32bb3e62344
Henrique <hndmedeiros@gmail.com>
2018-07-16 08:30:15 -0300

    added: jsp e servlet faltando

--- src/java/Controle/ExibirEmpresa.java
+++ src/java/Controle/ExibirEmpresa.java
@@ -5,19 +5,17 @@
  */
 package Controle;
 
-import DAO.Empresa;
-import DAO.EmpresaDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import DAO.EmpresaDAO;
+import Controle.ListaDeEmpresa;
 
 /**
  *
@@ -25,92 +23,30 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ExibirEmpresa", urlPatterns = {"/ExibirEmpresa"})
 public class ExibirEmpresa extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     * @throws java.sql.SQLException
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ExibirEmpresa</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ExibirEmpresa at " + request.getContextPath() + "</h1>");
-            out.println("<h1>Request recebida:" + request.getParameter("nomeEmpresa") + "////</h1>");
-            if (request.getParameter("nomeEmpresa") == null || request.getParameter("nomeEmpresa").equals("")) {
-                List<Empresa> empresas = EmpresaDAO.Listar();
-                for(Empresa e : empresas){
-                out.println("" 
-                        + "Empresa: " + e.GetNome() + "<br/>"
-                        + "CNPJ: " + e.GetCNPJ() + "<br/>"
-                        + "Validada: " + e.EstaValidada() + "<br/>"
-                        + "<hr>"
-                        + "<br/>");    
-                }
-                
-            }
-            out.println("</body>");
-            out.println("</html>");
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
             
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+    try{    
+            ListaDeEmpresa listaempresa = EmpresaDAO.Listar();
+            
+            if (listaempresa != null){
+                HttpSession session = request.getSession();
+                session.setAttribute("listaempresa", listaempresa);
+                request.setAttribute("listaempresa",listaempresa);
+                RequestDispatcher resposta = request.getRequestDispatcher("/ExibeEmpresa.jsp");
+                resposta.forward(request, response);
+            }else {
+                RequestDispatcher resposta = request.getRequestDispatcher("/Erro.jsp");
+                resposta.forward(request, response);
+            }
+          }catch (Exception e){
+    System.out.println("ERRO NA CONSULTA DE EMPRESA");
+}
+    }    
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(ExibirEmpresa.class.getName()).log(Level.SEVERE, null, ex);
-        }
+                response.sendRedirect("index.jsp");
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(ExibirEmpresa.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
+
---
+++ web/ExibeEmpresa.jsp
@@ -0,0 +1,72 @@
+<%@page import="Controle.ListaDeEmpresa"%>
+<%@page import="DAO.Empresa"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Lista de Empresas</title>
+        <link rel="stylesheet" type="text/css"  href="estilo.css" />
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    </head>
+    <body>
+                <img src= "imagem/uff.PNG">
+        <h1>Sistema de Estágios da UFF</h1> 
+        <nav>
+        <ul class="menu">
+        <li><a href="#">Home</a></li>
+            <li><a href="#">Perfil</a>
+                <ul>
+                      <li><a href="erro.jsp">Alterar senha</a></li>
+                      <li><a href="erro.jsp">Alterar contato</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Autorizar Pedidos</a>
+                <ul>
+                      <li><a href="autorizarEmpresa.jsp">Autorizar Empresa</a></li>
+                      <li><a href="erro.jsp">Autorizar Empresa</a></li>
+                      <li><a href="erro.jsp">Autorizar Vaga</a></li>
+                      <li><a href="erro.jsp">Autorizar Contrato</a></li>                    
+                </ul>
+            </li>
+        <li><a href="links.jsp">Links</a></li>
+        <li><a href="contato.jsp">Contato</a></li>     
+</ul>
+</nav><br><br> 
+<br>
+
+        h1>Lista de empresas do sistema:</h1>
+    
+                <table>
+            <%
+                ListaDeEmpresa resultado = (ListaDeEmpresa) request.getAttribute("listaempresanaoautorizado");
+            for (int i=0; i<resultado.getSize();i++){
+                Empresa aux= resultado.getEmpresa(i);
+            
+            %>
+            <tr>
+                <td>ID</td>
+                <td>LOGIN</td>
+                <td>NOME</td>
+                <td>CNPJ</td>
+                
+            </tr>
+            <tr>
+            <td> <%=aux.GetID()%></td>
+            <td><%=aux.GetLogin()%> </td>
+            <td><%=aux.GetNome()%> </td>
+            <td><%=aux.GetCNPJ()%> </td>
+            
+            <td><a href="AutorizarEmpresa?id=<%=aux.GetID()%>">Autorizar</a> </td>
+            
+            </tr>
+            
+            <%
+                }
+           %>
+            </table>
+            
+                <br><br>
+
+    </body>
+</html>
